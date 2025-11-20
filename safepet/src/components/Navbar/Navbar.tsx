452212b549ac003bd68f52efc1ec2a22d9766b6a
Giammi19 <amatrudagiammi@gmail.com>
2025-11-20 21:44:03 +0100

    Risoluzione conflitto

--- safepet/src/components/Navbar/Navbar.tsx
+++ safepet/src/components/Navbar/Navbar.tsx
@@ -136,11 +136,11 @@ function Navbar() {
               <span>Servizi <FontAwesomeIcon icon={faChevronDown} /></span>
               {openDropdown === 'servizi' && (
                 <div className="dropdown">
-                    <Link to="/ElencoVet">Elenco veterinari</Link>
-                    <Link to="/emergenze">Gestione emergenze</Link>
-                    <Link to="/prenotazioni">Prenotazioni veterinarie</Link>
-                    <Link to="/libretto">Libretto sanitario digitale</Link>
-                    <Link to="/mappa">Mappa veterinari</Link>
+                    <Link to="/ElencoVet" onClick={() => { setOpenDropdown(null); setIsMenuOpen(false); }}>Elenco veterinari</Link>
+                    <Link to="/emergenze" onClick={() => { setOpenDropdown(null); setIsMenuOpen(false); }}>Gestione emergenze</Link>
+                    <Link to="/prenotazioni" onClick={() => { setOpenDropdown(null); setIsMenuOpen(false); }}>Prenotazioni veterinarie</Link>
+                    <Link to="/libretto" onClick={() => { setOpenDropdown(null); setIsMenuOpen(false); }}>Libretto sanitario digitale</Link>
+                    <Link to="/mappa" onClick={() => { setOpenDropdown(null); setIsMenuOpen(false); }}>Mappa veterinari</Link>
                 </div>
               )}
             </div>
