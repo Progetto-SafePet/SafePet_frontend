d7bd9ed3e16975a27ce267ac0dd79ee82879ffa8
Francesco Maria Torino <80252595+FrancescoTorino1999@users.noreply.github.com>
2025-11-22 12:25:09 +0100

    Risoluzione conflitti

--- safepet/src/components/ElencoVet/ElencoVet.tsx
+++ safepet/src/components/ElencoVet/ElencoVet.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./ElencoVet.scss";
 import Title from "../Title/Title";
+import BannerHomepage from "../BannerHomepage/BannerHomepage";
+import Carousel from "../Carousel/Carousel";
+import Banner from "../Banner/Banner";
+import { useUser } from "../../Contexts/UserProvider";
 
 const StarRating = ({ rating }) => {
     const maxStars = 5;
@@ -22,6 +26,8 @@ const StarRating = ({ rating }) => {
 
 const ElencoVet = () => {
     const [Veterinari, setVeterinari] = useState([]);
+    const { usernameGlobal } = useUser();
+    
 
     useEffect(() => {
         const fetchVet = async () => {
@@ -46,34 +52,46 @@ const ElencoVet = () => {
     }, []);
 
     return (
-        <>
-            <Title text={"Lista veterinari"}/>
-        <div className="veterinario-container">
-            {Veterinari.length === 0 ? (
-                <p className="no-vet">Nessun veterinario presente</p>
-            ) : (
-                Veterinari.map((vet) => (
-                    <div key={vet.idVeterinario} className="veterinario-card">
-                        <div className="veterinario-image">
-                            <img src={`../imgs/vetPlaceholder.jpg`} alt={"Foto veterinario"}/>
-                        </div>
-
-                        <div className="veterinario-content">
-                            <h1 className="veterinario-title">{vet.nomeVeterinario} {vet.cognomeVeterinario}</h1>
-                            <div className="veterinario-description">
-                                <StarRating rating={Math.round(vet.mediaRecensioni)} />
-                                <div className={"nome-clinica"}>
-                                    {vet.nomeClinica} <br />
+        <div className="page-container">
+            <div className="page">
+                <div className='main-container'></div>
+                    <Title text={"Lista veterinari"}/>
+                    <div className="veterinario-container">
+                    {Veterinari.length === 0 ? (
+                        <p className="no-vet">Nessun veterinario presente</p>
+                    ) : (
+                        Veterinari.map((vet) => (
+                            <div key={vet.idVeterinario} className="veterinario-card">
+                                <div className="veterinario-image">
+                                    <img src={`../imgs/vetPlaceholder.jpg`} alt={"Foto veterinario"}/>
                                 </div>
-                                {vet.indirizzoClinica.replace(" - ", "\n")} <br />
-                                <strong>Telefono: </strong>{vet.telefonoClinica} <br />
+
+                                <div className="veterinario-content">
+                                    <h1 className="veterinario-title">{vet.nomeVeterinario} {vet.cognomeVeterinario}</h1>
+                                    <div className="veterinario-description">
+                                        <StarRating rating={Math.round(vet.mediaRecensioni)} />
+                                        <div className={"nome-clinica"}>
+                                            {vet.nomeClinica} <br />
+                                        </div>
+                                        {vet.indirizzoClinica.replace(" - ", "\n")} <br />
+                                        <strong>Telefono: </strong>{vet.telefonoClinica} <br />
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                ))
-            )}
+                        ))
+                    )}
+                    
+                </div>
+                {!usernameGlobal && (
+                    <Banner
+                        text="Registrati a SafePet per registrare il tuo pet e scoprire tutti i vantaggi"
+                        buttonText="Registrati"
+                        link = "/signup"
+                    >
+                    </Banner>
+                 )}
+            </div>
         </div>
-            </>
     );
 };
 
